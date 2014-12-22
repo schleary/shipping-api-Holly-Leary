5e5f26b8ba1817d87e195f3b1cc6d4c406baf3f5
Holly Leary <schleary@gmail.com>
2014-12-21 23:13:48 -0800

    timeout handling

--- app/controllers/shipments_controller.rb
+++ app/controllers/shipments_controller.rb
@@ -2,57 +2,73 @@ class ShipmentsController < ApplicationController
 
   #  require 'active_shipping'
   include ActiveMerchant::Shipping
+  rescue_from Timeout::Error, :with => :rescue_from_timeout
 
   def search
-    puts params.inspect
-    # Package up a poster and a Wii for your nephew.
 
-    @package = Package.new(  100,
-    [93,10],
-    :cylinder => true)
+    if params.blank?
+      render json: {error: "Must give complete address"}, status: :bad_request
+    else
+      # Package up a poster and a Wii for your nephew.
 
-    @origin =Location.new(
+      @package = Package.new(  100,
+      [93,10],
+      :cylinder => false)
 
-    # :country => params["country"],
-    #                       :state => params["state"],
-    #                       :city => params["city"],
-    #                       :zip => params["zip"])
+      @origin = Location.new(
 
-      :country => 'US',
-      :state => 'CA',
-      :city => 'Beverly Hills',
-      :zip => '90210')
+      # :country => params["country"],
+      #                       :state => params["state"],
+      #                       :city => params["city"],
+      #                       :zip => params["zip"])
 
-    @destination = Location.new(
-        :country => params["country"],
-        :state => params["state"],
-        :city => params["city"],
-        :zip => params["zip"])
+        :country => 'US',
+        :state => 'CA',
+        :city => 'Beverly Hills',
+        :zip => '90210'
+        # :options => {:shipping_date => DateTime.now}
+        )
 
-      # :country => 'CA',
-      # :province => 'ON',
-      # :city => 'Ottawa',
-      # :postal_code => 'K1P 1J1'
+      @destination = Location.new(
+      #     :country => params["country"],
+      #     :state => params["state"],
+      #     :city => params["city"],
+      #     :zip => params["zip"]
       # )
 
-    response = {}
-    ups = UPS.new(:login => ENV["UPS_LOGIN"], :password => ENV["UPS_PASSWORD"], :key => ENV["UPS_ACCESS_KEY"])
-    response[:ups] = ups.find_rates(@origin, @destination, @package)
+        :country => params["country"],
+        :province => params["state"],
+        :city => params["city"],
+        :postal_code => params["zip"]
+        # :options => {:shipping_date => DateTime.now}
+        )
 
-    # render json: response
+      response = {}
+      rates = {}
 
-    usps = USPS.new(:login => ENV["USPS_USERNAME"])
-    response[:usps] = usps.find_rates(@origin, @destination, @package)
+      ups = UPS.new(:login => ENV["UPS_LOGIN"], :password => ENV["UPS_PASSWORD"], :key => ENV["UPS_ACCESS_KEY"])
+      response[:ups] = ups.find_rates(@origin, @destination, @package)
+      rates[:ups_rates] = response[:ups].rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
 
-    # puts response.inspect
-    render json: response
+      usps = USPS.new(:login => ENV["USPS_USERNAME"])
+      response[:usps] = usps.find_rates(@origin, @destination, @package)
+      rates[:usps_rates] = response[:usps].rates.sort_by(&:price).collect {|rate| [rate.service_name, rate.price, rate.delivery_date]}
 
-    # fedex = FedEx.new(:login => ENV["FEDEX_LOGIN"], :password => ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"], :test => true)
-    # tracking_info = fedex.find_tracking_info('111111111111', :carrier_code => 'fedex_ground') # Ground package
-    # tracking_info.shipment_events.each do |event|
-    #   puts "#{event.name} at #{event.location.city}, #{event.location.state} on #{event.time}. #{event.message}"
-    # end
-    # render json: tracking_info
+      render json: rates, status: 200
 
+      # fedex = FedEx.new(:login => ENV["FEDEX_LOGIN"], :password => ENV["FEDEX_PASSWORD"], key: ENV["FEDEX_KEY"], account: ENV["FEDEX_ACCOUNT"], :test => true)
+      # tracking_info = fedex.find_tracking_info('111111111111', :carrier_code => 'fedex_ground') # Ground package
+      # tracking_info.shipment_events.each do |event|
+      #   puts "#{event.name} at #{event.location.city}, #{event.location.state} on #{event.time}. #{event.message}"
+      # end
+      # render json: tracking_info
+    end
   end
+
+  protected
+
+    def rescue_from_timeout(exception)
+      render json: {error: "Timeout Error"}, status: 408
+    end
+
 end
